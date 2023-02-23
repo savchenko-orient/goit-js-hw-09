5868d2ecfd1fd12b184e4d33fdde94752ceda2c3
Oleksii Savchenko <savchenko.orient@gmail.com>
2023-02-23 11:51:37 +0200

    complete task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const startBtn = document.querySelector("[data-start]");
+const stopBtn = document.querySelector("[data-stop]");
+const bodyEl = document.querySelector('body');
+let timer;
 
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function setBodyColor() {
+    timer = setInterval(() => {
+        bodyEl.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+}
+
+startBtn.addEventListener('click', () => {
+    setBodyColor();
+    startBtn.disabled = true;
+});
+
+stopBtn.addEventListener('click', () => {
+    clearInterval(timer);
+    startBtn.disabled = false;
+});
